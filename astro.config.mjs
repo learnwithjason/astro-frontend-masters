eabc688666bbd7ac1571e3d5a3064c2eed0a7a8f
Jason Lengstorf <jason@lengstorf.com>
2023-05-26 15:03:36 -0700

    feat: create a store and add framework support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import solidJs from "@astrojs/solid-js";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solidJs(), react()]
+});
