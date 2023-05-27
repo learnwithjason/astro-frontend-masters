caf3d7b4a2c13a83bb6b36f4969060688204ed98
Jason Lengstorf <jason@lengstorf.com>
2023-05-26 19:47:40 -0700

    feat: add a newsletter page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://astro-frontend-masters.netlify.app',
+	output: 'hybrid',
+	experimental: {
+		hybridOutput: true,
+	},
 	integrations: [solidJs(), react()],
 });
