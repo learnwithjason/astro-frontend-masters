8a46b92772a7ce5ec32634d27ef4fa871936977f
Jason Lengstorf <jason@lengstorf.com>
2023-05-26 18:49:09 -0700

    feat: add RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import solidJs from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), react()]
+	site: 'https://astro-frontend-masters.netlify.app',
+	integrations: [solidJs(), react()],
 });
