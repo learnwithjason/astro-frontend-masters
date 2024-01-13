8091188ce5cc920d6e189bced8f4a856ce047a48
suresh <sureshpradhana19@gmail.com>
2024-01-13 18:34:07 +0530

    cart

---
+++ src/components/add-to-cart.tsx
@@ -0,0 +1,10 @@
+/** @jsxImportSource react */
+
+import { addItemToCart } from "../stores/cart";
+
+export const AddToCart = ({ item }:{item:ShopItem}) => {
+    return (
+        <button  className="big-link" onClick={() => addItemToCart(item)}>Add to cart</button>
+    )
+}
+
---
+++ src/stores/cart.ts
@@ -0,0 +1,29 @@
+import {computed,map} from 'nanostores';
+
+
+export const $cart = map<Record<number,CartItem>>({});
+
+export function addItemToCart(item:ShopItem){
+    const cartItem=$cart.get()[item.id];
+    const quantity=cartItem?cartItem.quantity:0;
+    $cart.setKey(item.id,{
+        item,
+        quantity:quantity+1
+    });
+}
+export function removeItemFromCart(itemId:number){
+    //@ts-ignore
+    $cart.setKey(itemId,undefined);
+}
+
+export const subtotal = computed($cart, (entries) => {
+    let subtotal = 0;
+    Object.values(entries).forEach((entry) => {
+        if (!entry) {
+            return
+        }
+        subtotal += entry.item.price * entry.quantity;
+    });
+});
+
+
